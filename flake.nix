220e8b1c2502fc93ba12968437cd561cd476ff2d
Mason Mackaman <masondeanm@aol.com>
2021-03-21 23:40:23 -0400

    Add npm to the nix shell

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 { outputs = { self, nixpkgs, utils }:
     utils.mkShell
       ({ pkgs, ... }: with pkgs;
-        { buildInputs = [ nodePackages.node2nix ];
+        { buildInputs =
+            [ nodePackages.node2nix
+              nodePackages.npm
+            ];
 
           shellHook = ''alias node2nix="node2nix -d -l package-lock.json"'';
         }
